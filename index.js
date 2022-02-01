93ab439d093bd4bf0a12b45db4c09eea0e8bb95e
Tyler <tylerclancy030@gmail.com>
2022-02-01 13:58:58 -0600

    Create shared context

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ function Spa() {
   return (
     <HashRouter>
       <NavBar/>
-      <Route path="/" exact component={Home} />
-      <Route path="/CreateAccount/" component={CreateAccount} />
-      <Route path="/login/" component={Login} />
-      <Route path="/deposit/" component={Deposit} />
-      <Route path="/withdraw/" component={Withdraw} />
-      <Route path="/balance/" component={Balance} />
-      <Route path="/alldata/" component={AllData} />
+      <UserContext.Provider value={{users:[{
+        name: 'tyler',
+        email: 'tyler@mail.com',
+        password: 'secret',
+        balance: 100
+      }]}}>
+        <Route path="/" exact component={Home} />
+        <Route path="/CreateAccount/" component={CreateAccount} />
+        <Route path="/login/" component={Login} />
+        <Route path="/deposit/" component={Deposit} />
+        <Route path="/withdraw/" component={Withdraw} />
+        <Route path="/balance/" component={Balance} />
+        <Route path="/alldata/" component={AllData} />
+      </UserContext.Provider>
     </HashRouter>
   );
 }
