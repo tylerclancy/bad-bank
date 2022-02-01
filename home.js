9a35d9e95bf31a4e8be86fabf24a41a19fb05eaa
Tyler <tylerclancy030@gmail.com>
2022-02-01 16:27:17 -0600

    Add card to home page

--- home.js
+++ home.js
@@ -1,8 +1,11 @@
 function Home() {
-  const ctx = React.useContext(UserContext);
   return(
-    <h1>Home <br />
-      {JSON.stringify(ctx)}
-    </h1>
+    <Card
+      txtcolor="black"
+      header="BadBank Landing Page"
+      title="Welcome to the bank"
+      text="You can use this bank"
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)}
+    />
   );
 }
