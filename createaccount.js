e6525e4836c8ef4e5371a8db32f596c3242b081f
Tyler <tylerclancy030@gmail.com>
2022-02-01 11:37:07 -0600

    Verify routing

---
+++ createaccount.js
@@ -0,0 +1,5 @@
+function CreateAccount() {
+  return(
+    <h1>CreateAccount</h1>
+  );
+}
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function Spa() {
-  return(
-    <>
-    <h1>Welcome to Bad Bank!</h1>
-    <NavBar/>
-    </>
+  return (
+    <HashRouter>
+      <NavBar/>
+      <Route path="/" exact component={Home} />
+      <Route path="/CreateAccount/" component={CreateAccount} />
+      <Route path="/login/" component={Login} />
+      <Route path="/deposit/" component={Deposit} />
+      <Route path="/withdraw/" component={Withdraw} />
+      <Route path="/balance/" component={Balance} />
+      <Route path="/alldata/" component={AllData} />
+    </HashRouter>
   );
 }
 
-ReactDOM.render(<Spa />, document.getElementById('root'));
+ReactDOM.render(
+  <Spa />,
+  document.getElementById('root')
+);
