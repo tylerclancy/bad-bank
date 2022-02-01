96eced80177eb4a7855814cae9ab2d1020f390a4
Tyler <tylerclancy030@gmail.com>
2022-02-01 16:32:09 -0600

    Format and clean-up code

--- context.js
+++ context.js
@@ -6,18 +6,19 @@ const UserContext = React.createContext(null);
 function Card(props) {
   function classes() {
     const bg = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
-    const txt = props.txtcolor ? ' text-' + props.txtcolor: ' text-white';
+    const txt = props.txtcolor ? ' text-' + props.txtcolor : ' text-white';
     return 'card mb-3 ' + bg + txt;
   }
+
   return (
-    <div className={classes()} style={{maxwidth: '18rem'}}>
+    <div className={classes()} style={{ maxwidth: '18rem' }}>
       <div className="card-header">{props.header}</div>
       <div className="card-body">
-        {props.title && (<h5 className="card-title">{props.title}</h5>)}
-        {props.text && (<h5 className="card-text">{props.text}</h5>)}
+        {props.title && <h5 className="card-title">{props.title}</h5>}
+        {props.text && <h5 className="card-text">{props.text}</h5>}
         {props.body}
-        {props.status && (<div id='createStatus'>{props.status}</div>)}
+        {props.status && <div id="createStatus">{props.status}</div>}
       </div>
     </div>
-  )
+  );
 }
--- home.js
+++ home.js
@@ -1,11 +1,11 @@
 function Home() {
-  return(
+  return (
     <Card
       txtcolor="black"
       header="BadBank Landing Page"
       title="Welcome to the bank"
       text="You can use this bank"
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image" />)}
+      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
     />
   );
 }
