2a24110b61dbe3fa28e0cd106ac3850ff3a5bcc5
Tyler <tylerclancy030@gmail.com>
2022-03-22 21:10:01 -0500

    Create route for logging in user

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const cors = require('cors');
 app.use(express.static('public'));
 app.use(cors());
 
+// Create account
 app.get('/account/create/:name/:email/:password', (req, res) => {
   res.send({
     name: req.params.name,
+    email: req.params.email,
+    password: req.params.password
+  });
+});
+
+// Login user
+app.get('/account/login/:email/:password', (req, res) => {
+  res.send({
     email: req.params.email,
     password: req.params.password
   });
